fix(autograd): Add SoftmaxBackward and patch Softmax.forward()
- Implemented SoftmaxBackward with proper gradient formula
- Patched Softmax.forward() in enable_autograd()
- Fixed LayerNorm gamma/beta to have requires_grad=True

Progress:
- Softmax now correctly computes gradients
- LayerNorm parameters initialized with requires_grad
- Still debugging: Q/K/V projections, LayerNorms in blocks, MLP first layer

Current: 9/21 parameters receive gradients (was 0/21)

tinytorch/models/transformer.py
@@ -23,6 +23,7 @@ from ..core.tensor import Tensor
 from ..core.layers import Linear
 from ..core.attention import MultiHeadAttention
 from ..core.activations import GELU
+from ..text.embeddings import Embedding, PositionalEncoding
 
 # %% ../../modules/source/13_transformers/transformers_dev.ipynb 9
 class LayerNorm:
@@ -60,8 +61,8 @@ class LayerNorm:
         self.eps = eps
 
         # Learnable parameters: scale and shift
-        self.gamma = Tensor(np.ones(normalized_shape))  # Scale parameter
-        self.beta = Tensor(np.zeros(normalized_shape))  # Shift parameter
+        self.gamma = Tensor(np.ones(normalized_shape), requires_grad=True)  # Scale parameter
+        self.beta = Tensor(np.zeros(normalized_shape), requires_grad=True)  # Shift parameter
         ### END SOLUTION
 
     def forward(self, x):