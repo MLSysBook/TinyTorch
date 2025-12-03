e315e5648cd092f9341daeb94c940c3849ae9060
Vijay Janapa Reddi <vj@eecs.harvard.edu>
2025-12-02 19:23:15 -0500

    Remove VerifyCommand from exports

--- tito/commands/__init__.py
+++ tito/commands/__init__.py
@@ -29,7 +29,6 @@ __all__ = [
     'NBGraderCommand',
     'BenchmarkCommand',
     'CommunityCommand',
-    'VerifyCommand',
     # Command groups
     'SystemCommand',
     'ModuleWorkflowCommand',
